ac7f8530b89ed3c0c240fbe35d6e18c840e741e8
zx026 <yb131567@gmail.com>
2025-12-19 15:40:17 +0530

    Update config.py
    Signed-off-by: zx026 <yb131567@gmail.com>

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ class Config:
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
         self.OWNER_ID = int(getenv("OWNER_ID", 0))
 
-        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
-        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
-        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
+        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 240)) * 60
+        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 25))
+        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 30))
 
         self.SESSION1 = getenv("SESSION", None)
         self.SESSION2 = getenv("SESSION2", None)
